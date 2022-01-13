2e1c05af467fe8410cc636dba1e0fa1b5792ed5c
Nguyen manh <nguyentienmanhhotboy@gmail.com>
2022-01-13 22:11:37 +0700

    cập nhật all

--- src/main.js
+++ src/main.js
@@ -1,13 +1,34 @@
-const menuList = ['menu 1', 'menu 2', 'menu 3'];
+import Navigo from "navigo";
+import DetailNewsPage from "./pages/detailNews";
+import HomePage from "./pages/home";
 
-// b1: Selector
-// b2: loop
-// b3: render
-const menuElement = document.querySelector('#menu');
-if (menuElement) {
-  for (let i = 0; i < menuList.length; i++) {
-    menuElement.innerHTML += `<li>${menuList[i]}</li>`;
-  }
-}
-// template string
+const router = new Navigo("/", { linksSelector: "a" });
+const print = (content) => {
+    document.getElementById("app").innerHTML = content;
+};
 
+router.on({
+    "/": () => {
+        print(HomePage.render());
+    },
+    "/tsinh": () => {
+        print("Trang tuyển sinh");
+    },
+    "/ctdt": () => {
+        print("Trương trình đào tạo");
+    },
+    "/goctsinh": () => {
+        print("Góc tuyển sinh");
+    },
+    "/tdung": () => {
+        print("Tuyển dụng");
+    },
+    "/news/:id": (value) => {
+        console.log(value.data.id);
+        print(DetailNewsPage.render(value.data.id));
+    },
+});
+
+router.notFound(() => print("Not Found Page"));
+
+router.resolve();
